a499c9a8752ae81b9a056be907af29e47c9f05e6
nishanth1993 <44719075+nishanth1993@users.noreply.github.com>
2019-05-22 11:51:24 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 
 import PackageDescription
 
 
 #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
 let dependencies = [
-  Package.Dependency.package(url: "https://github.com/kylef-archive/CommonCrypto.git", from: "1.0.0"),
+  Package.Dependency.package(url: "https://github.com/nishanth1993/CommonCrypto.git", .branch("master")),
 ]
 let excludes = ["HMAC/HMACCryptoSwift.swift"]
 let targetDependencies: [Target.Dependency] = []
 #else
 let dependencies = [
-  Package.Dependency.package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.8.0"),
+  Package.Dependency.package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.0.0")),
 ]
 let excludes = ["HMAC/HMACCommonCrypto.swift"]
 let targetDependencies: [Target.Dependency] = ["CryptoSwift"]
